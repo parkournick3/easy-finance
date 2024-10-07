9e992e2e220f91f68ecf1edbf6553d76d426dc43
Nicolas <nicolasalexandre0001@gmail.com>
2024-10-07 19:42:05 -0300

    docs: document mvp routes

---
+++ apps/docs/pages/_meta.tsx
@@ -0,0 +1,10 @@
+export default {
+  index: "Get Started",
+  "docs-api": "API",
+  web: "Web App",
+  github_link: {
+    title: "GitHub",
+    href: "https://github.com/parkournick3/easy-finance",
+    newWindow: true,
+  },
+};
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -3,4 +3,9 @@ export default {
   project: {
     link: "https://github.com/parkournick3/easy-finance",
   },
+  docsRepositoryBase:
+    "https://github.com/parkournick3/easy-finance/tree/main/apps/docs",
+  footer: {
+    text: "Easy Finance Documentation",
+  },
 };
